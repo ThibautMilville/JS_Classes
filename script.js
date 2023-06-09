3aaff26da0bb06957a87f985c0732f9fd3cc5213
Thybow <tmilville.pro@gmail.com>
2023-06-09 18:13:02 +0200

    case

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Rectangle {
+/* class Rectangle {
     // Constructor to initialize the essential properties of the object
     constructor (w, h) {
         this.w = w;
@@ -22,4 +22,48 @@ class Square extends Rectangle {
     constructor(width) {
         super(width, width);
     }
+} */
+
+class Book {
+  #current_page = 1;
+  // Define the main variables
+  constructor(name, nb_pages) {
+    this.name = name;
+    this.nb_pages = nb_pages;
+  }
+  // Return the current page
+  get page () {
+    return this.#current_page;
+  }
+  // Increment #current_page to n+1
+  nextPage() {
+    if (this.#current_page < this.nb_pages) {
+      this.#current_page++;
+    }
+  }
+  // Reset page to 1
+  close() {
+    this.#current_page = 1;
+  }
+}
+
+class Library {
+  // Create an array (private to the class)
+  #books = [];
+  // Add a book
+  addBook (book) {
+    this.#books.push(book);
+  }
+  // Add several books
+  addBooks (books) {
+    this.#books.push(...books);
+  }
+  // Find a book by its first letter
+  findBooksByLetter (letter) {
+    const result = this.#books.filter(function(book) {
+      // Lowercase to avoid to have case problems
+      return book.name.toLowerCase().charAt(0) === letter.toLowerCase();
+    });
+    return result;
+  }
 }
